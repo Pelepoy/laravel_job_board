19501ad50ea16ab24b26797f4c66b9efc357fcbe
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-01-23 14:10:05 +0800

    Update card styling for hover background color

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<article {{ $attributes->class(["rounded-md border border-slate-300 bg-white p-6 shadow-sm hover:bg-slate-200"])}}> 
+<article {{ $attributes->class(["rounded-md border border-slate-300 bg-white p-6 shadow-sm hover:bg-slate-100"])}}> 
     {{ $slot }}
 </article>
