9a8b05aff94b362b2395aaed955b6b58ae8eacf8
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-01-24 19:21:39 +0800

    Refactor job-card.blade.php to include font-medium class in salary heading

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,7 +1,7 @@
 <x-card class="mb-4">
     <div class="mb-4 flex justify-between">
         <h1 class="text-lg font-medium"> {{ $job->title }}</h1>
-        <h1 class="text-slate-500"> <span class="font-semibold">₱</span>{{ number_format($job->salary) }} </h1>
+        <h1 class="text-slate-500 font-medium"> <span class="font-semibold">₱</span>{{ number_format($job->salary) }} </h1>
     </div>
     <div class="flex mb-4 justify-between text-slate-500 text-sm items-center">
         <div class="flex space-x-4">
@@ -13,9 +13,6 @@
             <x-tag>{{ $job->category }}</x-tag>
         </div>
     </div>
-    <p class="text-justify text-sm mb-4">
-        {!! nl2br(e($job->description)) !!}
-    </p>
 
     {{ $slot }}
 </x-card>
