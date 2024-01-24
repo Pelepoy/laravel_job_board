623f29aeae6377b795119e788d053df002a326dd
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-01-24 19:22:07 +0800

    Update job card to display job description

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,5 +2,9 @@
     <x-breadcrumbs class="mb-4" 
     :links="['Jobs' => route('jobs.index'), $job->title => '#']" 
     />
-    <x-job-card :job="$job" />
+    <x-job-card :job="$job"> 
+        <p class="text-justify text-sm mb-4">
+            {!! nl2br(e($job->description)) !!}
+        </p>
+    </x-job-card>
 </x-layout>
