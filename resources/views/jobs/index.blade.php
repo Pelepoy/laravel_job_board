d5ffdebf3c7340861d6b0bd528e812ba5ca84eea
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-01-24 19:22:17 +0800

    Refactor job search form in index.blade.php

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,22 +2,46 @@
     <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index')]" />
 
     <x-card class="mb-4 text-sm">
-        <div class="mb-4 grid grid-cols-2 gap-4">
-            <div>
-                <div class="mb-1 font-semibold">Search</div>
-                <x-text-input name="search" value="" placeholder="Search for any text"></x-text-input>
-            </div>
-            <div>
-                <div class="mb-1 font-semibold">Salary</div>
-                <div class="flex space-x-2">
-                    <x-text-input name="min_salary" value="" placeholder="From"></x-text-input>
-                    <x-text-input name="max_salary" value="" placeholder="To"></x-text-input>
+        <form action="{{ route('jobs.index') }}" method="GET">
+            <div class="mb-4 grid grid-cols-2 gap-4">
+                <div>
+                    <div class="mb-1 font-semibold">Search</div>
+                    <x-text-input name="search" value="{{ request('search') }}"
+                        placeholder="Search for any text"></x-text-input>
                 </div>
+                <div>
+                    <div class="mb-1 font-semibold">Salary</div>
+                    <div class="flex space-x-2">
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}"
+                            placeholder="From"></x-text-input>
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}"
+                            placeholder="To"></x-text-input>
+                    </div>
+                </div>
+                <div>
+                    <div class="mb-1 font-semibold">Experience</div>
 
+                    <label for="experience" class="mb-1 flex items-center">
+                        <input type="radio" name="experience" value="" @checked(!request('experience'))>
+                        <span class="ml-2">All</span>
+                    </label>
+                    <label for="experience" class="mb-1 flex items-center">
+                        <input type="radio" name="experience" value="Entry" @checked(request('experience') === 'Entry')>
+                        <span class="ml-2">Entry</span>
+                    </label>
+                    <label for="experience" class="mb-1 flex items-center">
+                        <input type="radio" name="experience" value="Intermediate" @checked(request('experience') === 'Intermediate')>
+                        <span class="ml-2">Intermediate</span>
+                    </label>
+                    <label for="experience" class="mb-1 flex items-center">
+                        <input type="radio" name="experience" value="Senior" @checked(request('experience') === 'Senior')>
+                        <span class="ml-2">Senior</span>
             </div>
-            <div>3</div>
             <div>4</div>
-        </div>
+            </div>
+            <button
+                class="w-full border bg-white text-slate-500 border-slate-300 rounded-md py-1.5 hover:bg-slate-200">Filter</button>
+        </form>
     </x-card>
     @foreach ($jobs as $job)
         <x-job-card class="mb-4" :job="$job">
