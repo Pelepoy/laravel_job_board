b953e50ff63c72e860aad85df244f5e8358f0cdd
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-06 11:02:03 +0800

    Refactor job-card.blade.php to include links for experience and category

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,16 +1,25 @@
 <x-card class="mb-4">
     <div class="mb-4 flex justify-between">
         <h1 class="text-lg font-medium"> {{ $job->title }}</h1>
-        <h1 class="text-slate-500 font-medium"> <span class="font-semibold">₱</span>{{ number_format($job->salary) }} </h1>
+        <h1 class="text-slate-500 font-medium"> <span class="font-semibold">₱</span>{{ number_format($job->salary) }}
+        </h1>
     </div>
     <div class="flex mb-4 justify-between text-slate-500 text-sm items-center">
         <div class="flex space-x-4">
-            <div>Company Name</div>
+            <div> {{$job->employer->company_name}}</div>
             <div> {{ $job->location }} </div>
         </div>
         <div class="flex space-x-1 text-xs">
-            <x-tag>{{ $job->experience }}</x-tag>
-            <x-tag>{{ $job->category }}</x-tag>
+            <x-tag>
+                <a href="{{ route('jobs.index', ['experience' => $job->experience]) }}">
+                    {{ Str::ucfirst($job->experience) }}
+                </a>
+            </x-tag>
+            <x-tag>
+                <a href="{{ route('jobs.index', ['category' => $job->category]) }}">
+                    {{ $job->category }}
+                </a>
+            </x-tag>
         </div>
     </div>
 
