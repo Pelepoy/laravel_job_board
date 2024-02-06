7974a43ecafb734372b95a6aa7c577940310d08b
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-06 11:03:14 +0800

    Add user and employer seeding, and update job seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Job;
+use App\Models\User;
+use App\Models\Employer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,6 +22,24 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        Job::factory(100)->create();
+        User::factory(300)->create();
+
+        $users = User::all()->shuffle();
+
+        for ($i = 0; $i < 20; $i++) {
+            Employer::factory()->create([
+                'user_id' => $users->pop()->id,
+            ]);
+        }
+
+        $employers = Employer::all();
+
+        for ($i = 0; $i < 100; $i++) {
+            Job::factory()->create([
+                'employer_id' => $employers->random()->id,
+            ]);
+        }
+
+        // Job::factory(100)->create();
     }
 }
