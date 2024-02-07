fae37535f9646ba57efc828f2e24c5ebde0f1127
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-07 18:35:33 +0800

    Refactor job show view and add related jobs section

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,10 +1,33 @@
 <x-layout>
-    <x-breadcrumbs class="mb-4" 
-    :links="['Jobs' => route('jobs.index'), $job->title => '#']" 
-    />
-    <x-job-card :job="$job"> 
+    <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index'), $job->title => '#']" />
+    <x-job-card :job="$job">
         <p class="text-justify text-sm mb-4">
             {!! nl2br(e($job->description)) !!}
         </p>
     </x-job-card>
+    <x-card class="mb-4">
+        <h2 class="mb-4 text-lg font-medium">
+            More {{ $job->employer->company_name }} Jobs you may interested in.
+        </h2>
+        <div class="text-sm text-slate-500">
+            @foreach ($job->employer->jobs as $otherJob)
+                <div class="flex justify-between mb-4">
+                    <div>
+                        <diV class="text-slate-500 font-medium hover:text-blue-500">
+                            <a href="{{route('jobs.show', $otherJob)}}">
+                                {{ $otherJob->title }}
+                            </a>
+                        </div>
+                        <div class="text-xs text-slate-400">
+                            {{ $otherJob->created_at->diffForHumans() }}
+                        </div>
+                    </div>
+                    <div class="text-xs">
+                        ₱{{ number_format($otherJob->salary) }}
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </x-card>
+
 </x-layout>
