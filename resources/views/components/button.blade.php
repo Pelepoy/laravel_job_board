8d580714125184b34fceb7cdd15b86dde8580fc6
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-15 16:23:55 +0800

    Update button styles

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,4 @@
 <button
-    {{ $attributes->class(['rounded-md border border-slate-500 bg-white px-2 py-1 text-center text-sm font-semibold hover:bg-slate-300']) }}>
+    {{ $attributes->class(['rounded-md px-2 py-1.5 shadow-md border-0 text-center text-white text-sm font-semibold bg-fuchsia-400 hover:bg-fuchsia-500']) }}>
     {{ $slot }}
 </button>
