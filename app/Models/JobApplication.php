c5b212ae92d71be6a316ec7081b71150d349ba82
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-15 16:22:04 +0800

    Add JobApplication model

---
+++ app/Models/JobApplication.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class JobApplication extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'expected_salary',
+        'user_id',
+        'job_id',
+    ];
+
+    public function job(): BelongsTo
+    {
+        return $this->belongsTo(Job::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
