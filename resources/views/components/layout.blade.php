b736fc66c584999863c8f0c048b87d523c11c92f
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-15 16:24:27 +0800

    Add navigation and success message to layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,41 @@
 </head>
 
 <body class="mx-auto mt-11 max-w-2xl bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200 text-slate-700">
+    <nav class="mb-8 flex justify-between text-lg font-medium">
+        <ul class="flex space-x-2">
+            <li>
+                <a href="{{route('jobs.index')}}">Home</a>
+            </li>
+        </ul>
+        <ul class="flex space-x-2 items-center">
+            @auth
+                <li>
+                    {{ auth()->user()->name ?? 'Anonymous' }}
+                </li>
+                <li>
+                    <form action="{{ route('auth.destroy') }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <x-button>Logout</x-button>
+                    </form>
+                </li>
+            @else
+                <li>
+                    <a href="{{ route('auth.create') }}">Sign Up</a>
+                </li>
+            @endauth
+        </ul>
+    </nav>
+
+    @if(session('success'))
+    <div x-data="{ show : true }" x-show="show" x-init="setTimeout(() => show = false, 5000)" role="alert" class="my-8 roundend-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
+        <p class="font-bold">Success!</p>
+        <p>{{session('success')}}</p>
+    </div>
+    @endif
+
     {{ $slot }}
+    
 </body>
 
 </html>
