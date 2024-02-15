3df3629af417a9251dabd000c2a92d46fb910e00
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-15 16:25:15 +0800

    Add routes for job application and my job application controllers

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Redirect;
+use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\MyJobApplicationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,10 +24,21 @@ use Illuminate\Support\Facades\Redirect;
 // });
 
 
-Route::redirect('/', 'jobs');
+Route::get('', fn () => to_route('jobs.index'));
 Route::resource('jobs', JobController::class)
     ->only(['index', 'show']);
 
-Route::redirect('login', 'auth/create')->name('login');
+Route::get('login', fn () => to_route('auth.create'))->name('login');
 Route::resource('auth', AuthController::class)
     ->only(['create', 'store']);
+
+Route::delete('logout', fn () => to_route('auth.destroy'))->name('logout');
+Route::delete('auth', [AuthController::class, 'destroy'])->name('auth.destroy');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('job.application', JobApplicationController::class)
+        ->only(['create', 'store']);
+
+    Route::resource('my-job-application', MyJobApplicationController::class)
+        ->only(['index', 'destroy']);
+});
