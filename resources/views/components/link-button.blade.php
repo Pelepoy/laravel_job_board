50b698a79150d5ac4554360637a5a7cb996c7489
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-15 16:24:44 +0800

    Refactor link-button component to use class attribute chaining

--- resources/views/components/link-button.blade.php
+++ resources/views/components/link-button.blade.php
@@ -1,4 +1,3 @@
-<a href="{{ $href }}"
-    class="rounded-md border border-slate-500 bg-white px-2 py-1 text-center text-sm font-semibold hover:bg-slate-300">
+<a href="{{ $href }}" {{ $attributes->class(["rounded-md border border-slate-500 bg-white px-2 py-1 text-center text-sm font-semibold hover:bg-slate-300"]) }}>
    {{ $slot }}
 </a>
