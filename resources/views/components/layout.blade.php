1ad45efa44b830c879ed5177186a81c4c70cf14d
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-16 17:09:38 +0800

    Update user name link and success message timeout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,7 +19,9 @@
         <ul class="flex space-x-2 items-center">
             @auth
                 <li>
-                    {{ auth()->user()->name ?? 'Anonymous' }}
+                    <a href="{{ route('my-job-application.index') }}">
+                        {{ auth()->user()->name }}: Applications
+                    </a>
                 </li>
                 <li>
                     <form action="{{ route('auth.destroy') }}" method="POST">
@@ -37,7 +39,7 @@
     </nav>
 
     @if(session('success'))
-    <div x-data="{ show : true }" x-show="show" x-init="setTimeout(() => show = false, 5000)" role="alert" class="my-8 roundend-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
+    <div x-data="{ show : true }" x-show="show" x-init="setTimeout(() => show = false, 3000)" role="alert" class="my-8 roundend-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
         <p class="font-bold">Success!</p>
         <p>{{session('success')}}</p>
     </div>
