198a584b3409c9ddf448a6cd6ec8800f26651a20
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-25 18:03:26 +0800

    Refactor label component to include required indicator

--- resources/views/components/label.blade.php
+++ resources/views/components/label.blade.php
@@ -1,3 +1,4 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+<label for="{{ $for }}" class="mb-2 block text-small font-medium text-slate-700">
+    {{ $slot }} @if ($required) <span class="text-red-500">*</span> 
+    @endif
+</label>
