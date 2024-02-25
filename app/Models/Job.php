3bdb5fc484c7ede34142c5776f31a1e344653ede
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-25 18:04:23 +0800

    Add fillable properties to Job model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -17,6 +17,15 @@ class Job extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'location',
+        'salary',
+        'description',
+        'experience',
+        'category',
+    ];
+
     public static array $experience = [
         'entry',
         'intermediate',
